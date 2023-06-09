5ec1353838e3acdfa45a832c7a90e2de6bc36593
a3372djan <135830467+a3372djan@users.noreply.github.com>
2023-06-08 22:25:00 -0700

    Add files via upload

---
+++ ex05_rectangle.py
@@ -0,0 +1,20 @@
+# this progran print a table using for loop
+def table_for():
+    for i in range(1,11):
+        print (f"{i}x{a}={i*a}")
+def table_print():
+    print(f"{i}x{a}={1*a}")
+    print(f"{i}x{a}={2*a}")
+    print(f"{i}x{a}={3*a}")
+    print(f"{i}x{a}={4*a}")
+    print(f"{i}x{a}={5*a}")
+    print(f"{i}x{a}={6*a}")
+    print(f"{i}x{a}={7*a}")
+    print(f"{i}x{a}={8*a}")
+    print(f"{i}x{a}={9*a}")
+    print(f"{i}x{a}={10*a}")
+
+a=int(input("enter table ="))
+
+
+table_for()
