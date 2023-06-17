4fa274f80e31c4c55ed7b1692198207811d88413
a3372djan <135830467+a3372djan@users.noreply.github.com>
2023-06-16 22:46:38 -0700

    Add files via upload

---
+++ ex12.py
@@ -0,0 +1,32 @@
+class Arthmetic:
+    def add(a,b):
+        result=a+b
+        return result
+
+    def subtract(a,b):
+        result=a-b
+        return result
+
+
+    def multiply(x,y):
+        result=x*y
+        return result
+
+    def divide(w,v):
+        result=w/v
+        return result
+
+
+    def reminder(w,v):
+        result=w%v
+        return result
+
+
+    def percentase(m,t):
+        result=m*100/t
+        return result
+    def hello(n):
+        print(f"hello and wellcome {n}")
+
+obj=Arthmetic
+obj.hello("Ahmad")
